9f38a84a34da358ce6bc3c99c6d31d4f4affd88c
Laura Hope <ssteamlaura@gmail.com>
2023-04-12 10:28:33 -0700

    add first passing test for character class name.

---
+++ src/js/character.js
@@ -0,0 +1,40 @@
+
+export default class Character {
+  constructor(name, characterClass, strength, intelligence, dexterity, maxHealth, currentHealth, level, experience, money) {
+    this.name = name;
+    // this.characterClass = characterClass;
+    // this.strength = strength;
+    // this.intelligence = intelligence;
+    // this.dexterity = dexterity;
+    // this.maxHealth = maxHealth;
+    // this.currentHealth = currentHealth;
+    // this.level = level;
+    // this.experience = experience;
+    // this.money = money;
+    // this.inventory = new Inventory(10);
+  }
+}
+
+
+
+
+
+// export default class Triangle {
+//   constructor(side1, side2, side3) {
+//     this.side1 = side1;
+//     this.side2 = side2;
+//     this.side3 = side3;
+//   }
+
+//   checkType() {
+//     if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+//       return "not a triangle";
+//     } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+//       return "scalene triangle";
+//     }  else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+//       return "equilateral triangle";
+//     } else {
+//       return "isosceles triangle";
+//     }
+//   }    
+// }
