fc5e088b432ca45568c2a1e00ae040a1dd098038
Laura Hope <ssteamlaura@gmail.com>
2023-04-12 09:31:09 -0700

    Test commit.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Triangle from './js/triangle.js';
 import Rectangle from './js/rectangle.js';
-
+ 
 function handleTriangleForm() {
   event.preventDefault();
   document.querySelector('#response').innerText = null;
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'RPG',
       template: './src/index.html',
       inject: 'body'
     })
